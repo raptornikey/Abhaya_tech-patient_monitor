8ed77615a5b0b3abeca9430e16780c6e1827ba9e
NIKHIL SHRIRAM RAVICHANDREN <72442136+raptornikey@users.noreply.github.com>
2020-12-18 12:35:02 +0530

    SpO2 Vino code

--- main.c
+++ main.c
@@ -1,253 +1,131 @@
-/**
- * Copyright (c) 2014 - 2020, Nordic Semiconductor ASA
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form, except as embedded into a Nordic
- *    Semiconductor ASA integrated circuit in a product or a software update for
- *    such product, must reproduce the above copyright notice, this list of
- *    conditions and the following disclaimer in the documentation and/or other
- *    materials provided with the distribution.
- *
- * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * 4. This software, with or without modification, must only be used with a
- *    Nordic Semiconductor ASA integrated circuit.
- *
- * 5. Any software provided in binary form under this license must not be reverse
- *    engineered, decompiled, modified and/or disassembled.
- *
- * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-/** @file
- * @defgroup nrf_adc_example main.c
- * @{
- * @ingroup nrf_adc_example
- * @brief ADC Example Application main file.
- *
- * This file contains the source code for a sample application using ADC.
- *
- * @image html example_board_setup_a.jpg "Use board setup A for this example."
- */
 
-#include <stdbool.h>
-#include <stdint.h>
+
+
 #include <stdio.h>
-#include <string.h>
-#include "nrf.h"
-#include "nrf_drv_saadc.h"
-#include "nrf_drv_ppi.h"
-#include "nrf_drv_timer.h"
 #include "boards.h"
-#include "app_error.h"
-#include "nrf_delay.h"
 #include "app_util_platform.h"
-#include "nrf_pwr_mgmt.h"
+#include "app_error.h"
+#include "nrf_drv_twi.h"
+#include "nrf_delay.h"
+
 
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
+#include "max30105.h"
+//#include "max30105.c"
 
-#define SAMPLES_IN_BUFFER 1
-#define RELAY_PIN 8
-volatile uint8_t state = 1;
-
-
-static const nrf_drv_timer_t m_timer = NRF_DRV_TIMER_INSTANCE(0);
-static nrf_saadc_value_t     m_buffer_pool[1][SAMPLES_IN_BUFFER];
-static nrf_ppi_channel_t     m_ppi_channel;
-static uint32_t              m_adc_evt_counter;
-static uint32_t              arrayData[40];
-static uint8_t               counter = 0; 
-static bool                  RELAY_STATUS = false;
+/* TWI instance ID. */
+#define TWI_INSTANCE_ID     0
 
 
 
-void timer_handler(nrf_timer_event_t event_type, void * p_context)
+/* Mode for LM75B. */
+#define NORMAL_MODE 0U
+
+/* Indicates if operation on TWI has ended. */
+
+
+/* TWI instance. */
+static const nrf_drv_twi_t m_twi = NRF_DRV_TWI_INSTANCE(TWI_INSTANCE_ID);
+
+/* Buffer for samples read from  sensor. */
+
+__STATIC_INLINE void data_handler(uint32_t data)
 {
-
+    NRF_LOG_INFO("Data returned: %d", data);
 }
 
-
-void saadc_sampling_event_init(void)
+/**
+ * @brief TWI events handler.
+ */
+void twi_handler(nrf_drv_twi_evt_t const * p_event, void * p_context)
 {
-    ret_code_t err_code;
-
-    err_code = nrf_drv_ppi_init();
-    APP_ERROR_CHECK(err_code);
-
-    nrf_drv_timer_config_t timer_cfg = NRF_DRV_TIMER_DEFAULT_CONFIG;
-    timer_cfg.bit_width = NRF_TIMER_BIT_WIDTH_32;
-    err_code = nrf_drv_timer_init(&m_timer, &timer_cfg, timer_handler);
-    APP_ERROR_CHECK(err_code);
-
-    /* setup m_timer for compare event every 400ms */
-    uint32_t ticks = nrf_drv_timer_ms_to_ticks(&m_timer, 250);
-    nrf_drv_timer_extended_compare(&m_timer,
-                                   NRF_TIMER_CC_CHANNEL0,
-                                   ticks,
-                                   NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK,
-                                   false);
-    nrf_drv_timer_enable(&m_timer);
-
-    uint32_t timer_compare_event_addr = nrf_drv_timer_compare_event_address_get(&m_timer,
-                                                                                NRF_TIMER_CC_CHANNEL0);
-    uint32_t saadc_sample_task_addr   = nrf_drv_saadc_sample_task_get();
-
-    /* setup ppi channel so that timer compare event is triggering sample task in SAADC */
-    err_code = nrf_drv_ppi_channel_alloc(&m_ppi_channel);
-    APP_ERROR_CHECK(err_code);
-
-    err_code = nrf_drv_ppi_channel_assign(m_ppi_channel,
-                                          timer_compare_event_addr,
-                                          saadc_sample_task_addr);
-    APP_ERROR_CHECK(err_code);
-}
-
-
-void saadc_sampling_event_enable(void)
-{
-    ret_code_t err_code = nrf_drv_ppi_channel_enable(m_ppi_channel);
-
-    APP_ERROR_CHECK(err_code);
-}
-
-
-void saadc_callback(nrf_drv_saadc_evt_t const * p_event)
-{
-    if (p_event->type == NRF_DRV_SAADC_EVT_DONE)
+    switch (p_event->type)
     {
-        ret_code_t err_code;
-
-        err_code = nrf_drv_saadc_buffer_convert(p_event->data.done.p_buffer, SAMPLES_IN_BUFFER);
-        APP_ERROR_CHECK(err_code);
-        float Dia=0,Sys=0,pressureMax=15,pressureMin=-15,Vsupply=5,volta=0,maxvolt=0,volt=0,pressure=0,MAP=0,maxv=0;
-        int i;
-
-       // NRF_LOG_INFO("ADC event number: %d", (int)m_adc_evt_counter);
-        if(!RELAY_STATUS){
-            RELAY_STATUS = true;
-            nrf_gpio_pin_set(RELAY_PIN);
+        case NRF_DRV_TWI_EVT_DONE:
+            if (p_event->xfer_desc.type == NRF_DRV_TWI_XFER_RX)
+            {
+                data_handler(m_sample);
             }
-        for (i = 0; i < SAMPLES_IN_BUFFER && counter < 40; i++)
-        {
-          /*  if(!RELAY_STATUS){
-            RELAY_STATUS = true;
-            nrf_gpio_pin_set(RELAY_PIN);
-            }else{
-             RELAY_STATUS = false;
-             nrf_gpio_pin_clear(RELAY_PIN);
-            }*/
-           
-            //arrayData[counter] = p_event->data.done.p_buffer[i];
-            
-            volta=(p_event->data.done.p_buffer[i]);
-            //NRF_LOG_INFO("%d, Volt",volta);
-            volt=((volta*5)/(1023.00));
-            maxv=max(abs(volt-2.5),maxvolt);
-            maxvolt=abs(maxv-2.5);
-            pressure=(((maxvolt)-(0.1*Vsupply))/((0.8*Vsupply)/(pressureMax-pressureMin)))+pressureMin;
-           // MAP=5;
-            MAP=(((-1*(14.7-pressure*-1))*51.7)-(3.16/maxvolt));
-            //NRF_LOG_INFO("%d", counter);
-            //NRF_LOG_INFO("%d", p_event->data.done.p_buffer[i]);
-            MAP*=-1;
-            Sys=MAP*1.1;
-            Dia=MAP*0.8;
-            
-            if(counter == 39){
-             NRF_LOG_INFO("Mean Arterial Pressure=%d  ", MAP);
-             NRF_LOG_INFO("Systolic=%d  ", Sys);
-             NRF_LOG_INFO("Diastolic=%d  \n", Dia);        
-            }
-            counter++;
-
-           }
-        }
-           
-
-             if(RELAY_STATUS){
-             RELAY_STATUS = false;
-             nrf_gpio_pin_clear(RELAY_PIN);
-            }
-           
-        
-        // nrf_gpio_pin_write(ARDUINO_10_PIN,0);
-        // m_adc_evt_counter++;
+            m_xfer_done = true;
+            break;
+        default:
+            break;
     }
 }
 
-
-void saadc_init(void)
+/**
+ * @brief UART initialization.
+ */
+void twi_init (void)
 {
     ret_code_t err_code;
-    nrf_saadc_channel_config_t channel_config =
-        NRF_DRV_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_AIN0);
 
-    err_code = nrf_drv_saadc_init(NULL, saadc_callback);
+    const nrf_drv_twi_config_t twi_MAX30105_config = {
+       .scl                = ARDUINO_SCL_PIN,
+       .sda                = ARDUINO_SDA_PIN,
+       .frequency          = NRF_TWI_FREQ_400K,
+       .interrupt_priority = APP_IRQ_PRIORITY_HIGH,
+       .clear_bus_init     = false
+    };
+
+    err_code = nrf_drv_twi_init(&m_twi, &twi_MAX30105_config, twi_handler, NULL);
     APP_ERROR_CHECK(err_code);
 
-    err_code = nrf_drv_saadc_channel_init(0, &channel_config);
-    APP_ERROR_CHECK(err_code);
-
-    err_code = nrf_drv_saadc_buffer_convert(m_buffer_pool[0], SAMPLES_IN_BUFFER);
-    APP_ERROR_CHECK(err_code);
-
-    /* err_code = nrf_drv_saadc_buffer_convert(m_buffer_pool[1], SAMPLES_IN_BUFFER);
-    APP_ERROR_CHECK(err_code); */
-
+    nrf_drv_twi_enable(&m_twi);
 }
 
-float max(float a,float b){
-if(a>b) return(a);
-else return(b);}
+/**
+ * @brief Function for reading data from temperature sensor.
+ */
+//static void read_sensor_data()
+//{
+ //   m_xfer_done = false;
+
+    /* Read 1 byte from the specified address - skip 3 bits dedicated for fractional part of temperature. */
+  //  ret_code_t err_code = nrf_drv_twi_rx(&m_twi, LM75B_ADDR, &m_sample, sizeof(m_sample));
+  //  APP_ERROR_CHECK(err_code);
+//}
+
 /**
  * @brief Function for main application entry.
  */
 int main(void)
 {
-    uint32_t err_code = NRF_LOG_INIT(NULL);
-    APP_ERROR_CHECK(err_code);
-
+    APP_ERROR_CHECK(NRF_LOG_INIT(NULL));
     NRF_LOG_DEFAULT_BACKENDS_INIT();
 
-    ret_code_t ret_code = nrf_pwr_mgmt_init();
-    APP_ERROR_CHECK(ret_code);
+    NRF_LOG_INFO("\r\nTWI sensor example started.");
+    NRF_LOG_FLUSH();
+    twi_init();
+    NRF_LOG_INFO("\r\nTWI init finished.");
+    NRF_LOG_FLUSH();
+    MAX30105_set_mode();
+    NRF_LOG_INFO("\r\nMAX30105 set mode finished.");
+    NRF_LOG_FLUSH();
+    setup(6.4,4,3,50,411,16384);
+    my_timer_start();
 
-
-   nrf_gpio_cfg_output(RELAY_PIN); 
- 
-   
-
-    saadc_init();
-    saadc_sampling_event_init();
-    saadc_sampling_event_enable();
-    NRF_LOG_INFO("SAADC HAL simple example started.");
-
-    while (1)
+    while (true)
     {
-        nrf_pwr_mgmt_run();
+        nrf_delay_ms(500);
+
+        do
+        {
+            __WFE();
+        }while (m_xfer_done == false);
+
+        int ir = getIR();
+        NRF_LOG_INFO("IR : %d",ir);
+        int red = getRed();
+        NRF_LOG_INFO("RED : %d",red);
+        int green = getGreen();
+        NRF_LOG_INFO("GREEN : %d",green);
+
         NRF_LOG_FLUSH();
     }
 }
-
-
 /** @} */
+
+
